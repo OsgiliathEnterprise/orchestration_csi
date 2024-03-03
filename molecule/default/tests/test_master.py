c04d07eeb1c2f407f5db239558a2b8cdeed4488d
Charlie Mordant <cmordant1@gmail.com>
2024-03-03 23:43:09 +0100

    [skip travis] updates to last commit
    Signed-off-by: Charlie Mordant <cmordant1@gmail.com>

--- molecule/default/tests/test_master.py
+++ molecule/default/tests/test_master.py
@@ -11,7 +11,8 @@ def test_nfs_provisioner_helm_chart_created(host):
 
 
 def test_nfs_provisioner_pod_is_running(host):
-    command = """kubectl get po -n nfs-subdir-provisioner | \
+    command = """sleep 10 & \
+    kubectl get po -n nfs-subdir-provisioner | \
     grep nfs-subdir | \
     grep -c 'Running'"""
     with host.sudo():
